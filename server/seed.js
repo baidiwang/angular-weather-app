99d9d0b54cb4c44c49e11e21cb61132b72f41783
HinkRoy <55680212+baidiwang@users.noreply.github.com>
2024-03-02 21:16:31 +0800

    增加seed

---
+++ server/seed.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const Favorite = require("./models/favorite");
+
+const MONGODB_URL = 'mongodb+srv://baidiwang:baidiwang12345@cluster0.0mjpheu.mongodb.net/';
+
+const initialFavorites = [
+  {
+    country: "United States of America",
+    lat: 40.71,
+    lon: -74.01,
+    name: "New York",
+    region: "New York",
+    tz_id: "America/New_York",
+  },
+  {
+    country: "China",
+    lat: 31.01,
+    lon: 121.41,
+    name: "Shanghai",
+    region: "Shanghai",
+    tz_id: "Asia/Shanghai",
+  }
+]
+
+async function init() {
+  await mongoose.connect(MONGODB_URL);
+
+  await Favorite.deleteMany({});
+
+  for (let i = 0; i < initialFavorites.length; i++) {
+    const favorite = new Favorite(initialFavorites[i]);
+    await favorite.save();
+  }
+
+  console.log('MongoDB Connected!');
+}
+
+
+init().then(() => {
+  console.log('Data inserted');
+})
